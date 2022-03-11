5dba442595fb9792a1a5a0995026206d35574177
wyj <553495960@qq.com>
2022-03-11 23:56:57 +0800

    Home 2022-3-11 23:56:50

---
+++ src/main/java/com/wyj/algorithm/example/sliding_windows/MinWindowSubstring.java
@@ -0,0 +1,14 @@
+package com.wyj.algorithm.example.sliding_windows;
+
+/**
+ * @Author Wuyj
+ * @DateTime 2022-03-11 23:47
+ * @Version 1.0
+ */
+public class MinWindowSubstring {
+    //方法一:暴力法,枚举s中所有子串
+    //先不管最小和覆盖,得保证是字串
+    public String minWindow(String s, String t) {
+        return null;
+    }
+}
